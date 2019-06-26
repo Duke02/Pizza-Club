c44cbc2083ea3b2fc42bb80e98f0d40ed1d357f3
Trystan May <trystanmay@gmail.com>
2019-06-25 20:43:13 -0500

    Added arguments and loading csv

--- analyze_pizza.py
+++ analyze_pizza.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
+import numpy as np
+import pandas as pd
+from argparse import Namespace, ArgumentParser
+
+def load_csv(file_name: str) -> pd.DataFrame:
+	""" Loads and prunes the desired csv. """
+	output: pd.DataFrame = pd.read_csv(file_name, index_col=0)
+	return output
+
+def parse_args() -> Namespace:
+	arg_parser = ArgumentParser(description='Analyze Pizza Club ratings.')
+	arg_parser.add_argument('--data-file', required=True, help='.csv file that contains pizza club ratings.', type=str)
+	return arg_parser.parse_args()
+
+def main():
+	arguments: Namespace = parse_args()
+	data: pd.DataFrame = load_csv(arguments.data_file)
+	print(data)
+
 if __name__ == "__main__":
-	print("Hello World!")
+	main()
